28ef17da266d03e11206365c4576712e4112125f
Nirad Yeola <yeola.nirad@gmail.com>
2023-09-03 10:39:04 +0530

    initiated prj

---
+++ lib/home_screen.dart
@@ -0,0 +1,95 @@
+import "package:carousel_slider/carousel_slider.dart";
+import "package:flutter/material.dart";
+
+
+class HomeScreen extends StatelessWidget {
+  final List<String> categories = ['Shows', 'Movies', 'Live Sports'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EthFlix'),
+        // backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Handle search action
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return CategorySection(category: categories[index]);
+        },
+      ),
+    );
+  }
+}
+
+class CategorySection extends StatelessWidget {
+  final String category;
+
+  CategorySection({required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            category,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        CarouselSlider(
+          options: CarouselOptions(
+            autoPlay: true,
+            height: 200,
+            enlargeCenterPage: true,
+          ),
+          items: [
+            CardWidget(imageUrl: 'https://img2.wallspic.com/previews/0/9/9/7/3/137990/137990-uefa_champions_league-sky-lionel_messi-sport_venue-fun-x750.jpg'),
+            CardWidget(imageUrl: 'https://img2.wallspic.com/previews/0/9/9/7/3/137990/137990-uefa_champions_league-sky-lionel_messi-sport_venue-fun-x750.jpg'),
+            CardWidget(imageUrl: 'https://img2.wallspic.com/previews/0/9/9/7/3/137990/137990-uefa_champions_league-sky-lionel_messi-sport_venue-fun-x750.jpg'),
+            // Add more CardWidgets for each item in the carousel
+          ],
+        ),
+        SizedBox(height: 20),
+      ],
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final String imageUrl;
+
+  CardWidget({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+            width: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
