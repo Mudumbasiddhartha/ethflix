19bebcadd0813aacc9accbe9b8622a5cb4a33984
Nirad Yeola <yeola.nirad@gmail.com>
2023-10-24 00:25:54 +0530

    successfully deployed contracts on ganache

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:appinio_video_player/appinio_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const PaymentProcessor = artifacts.require("PaymentProcessor.sol");
 
 module.exports = async function (deployer, network, addresses) {
     const [admin, payer, _] = addresses;
-    if(network === 'develop') {
+    if(network === 'development') {
         await deployer.deploy(Dai);
         const dai = await Dai.deployed();
         await dai.faucet(payer, web3.utils.toWei('10000')); // minting 10000 DAI tokens
